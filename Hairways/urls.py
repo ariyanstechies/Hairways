2aa75729c903a047f1fc7b6ae3ac226b76bcfe59
dennohpeter <denispeterson96@gmail.com>
2018-11-23 13:59:48 +0300

    Almost Done with Registration

--- Hairways/urls.py
+++ Hairways/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$|^index/$', views.index, name='index'),
     url(r'^faqs/$', views.faqs, name='faqs'),
     url(r'^about/$', views.about, name='about'),
-    url(r'^login/$', views.login, name='login'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^pricing/$', views.pricing, name='pricing'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^moreinfo/$', views.moreinfo, name='moreinfo'),
--- Hairways/views.py
+++ Hairways/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from Hairways.forms import SignUpForm
 
 
 def index(request):
@@ -13,8 +15,23 @@ def about(request):
     return render(request, "about.html")
 
 
-def login(request):
-    return render(request, "login.php")
+# def login(request):
+#     return render(request, "login.php")
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
 
 
 def dashboard(request):

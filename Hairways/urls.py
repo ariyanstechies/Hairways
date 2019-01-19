94fe331a829f74df236353f738536d9bff9b642b
Peterson <denispeterson96@gmail.com>
2019-01-19 17:25:02 +0300

    Dealing Proceess

--- Hairways/urls.py
+++ Hairways/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views

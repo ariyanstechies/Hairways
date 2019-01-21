ff53a1cf2732d3bc9384969d9c1e46ae4d89cfd4
Peterson <denispeterson96@gmail.com>
2019-01-21 14:21:49 +0300

    Removed Unnecessary Urls

--- Hairways/urls.py
+++ Hairways/urls.py
@@ -5,12 +5,8 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    url(r'^$|^home/$', views.home, name='home'),
-    url(r'^faqs/$', views.faqs, name='faqs'),
+    url(r'^$', views.home, name='home'),
     url(r'^about/$', views.about, name='about'),
-    # url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    # url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
-    url(r'^signup/$', views.signup, name='signup'),
     url(r'^pricing/$', views.pricing, name='pricing'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^moreinfo/$', views.moreinfo, name='moreinfo'),
@@ -21,6 +17,7 @@ urlpatterns = [
     url(r'^calendar/$', views.calendar, name='calendar'),
     url(r'^upgrade/$', views.upgrade, name='upgrade'),
     url(r'^upload/$', views.upload, name='upload'),
+    url(r'^faqs/$', views.faqs, name='faqs'),
     ]
 
 if settings.DEBUG:

8b2d7e669d53c6588e4159f262aea386843ba7cf
Peterson <denispeterson96@gmail.com>
2019-01-21 14:18:39 +0300

    Removing Junk

--- Base/urls.py
+++ Base/urls.py
@@ -4,5 +4,4 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Hairways.urls")),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]

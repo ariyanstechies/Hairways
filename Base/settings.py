606151582821d3115a04641c14be36aa1a349019
Peterson <denispeterson96@gmail.com>
2019-01-23 00:11:50 +0300

    Password Reset Complete

--- Base/settings.py
+++ Base/settings.py
@@ -114,8 +114,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# user redirected to home after successfull login
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+# prints emails on the console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 

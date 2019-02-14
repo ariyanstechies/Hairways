669e81de3dd99288a10aa076d842e165865fa2b2
Peterson <peterson@localhost.localdomain>
2019-02-15 01:05:43 +0300

    Commented moreinfo view method

--- Hairways/views/home_views.py
+++ Hairways/views/home_views.py
@@ -94,17 +94,7 @@ def upgrade(request):
 
 
 def moreinfo(request, id):
-    """django takes care of joins automatically. When we query everything in Salons
-    we also get any related information by foreign key as well,
-     But the way that we access this in html is a litle different.
-      We have to go through the variable used as the foreign key to access the
-       information associated with that join. e.g
-       {% for x in data %}
-       {{ x.salonName.serviceName }}
-       {{ x.salonName.availability }} etc... easy right? :)
-       {% endfor %}
-       """
-    salon = Salons.objects.get(id=id)
+    salon = Salons.objects.get(id=id) # moving to Salons model, and getting salonNameand then filtering it
     services = Services.objects.filter(salons__salonName=salon.salonName)
     products = Products.objects.filter(salons__salonName=salon.salonName)
     return render(request, "moreinfo.html", {'salon': salon, 'services' : services, 'products' : products})

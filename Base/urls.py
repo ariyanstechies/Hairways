89dc34433a372319dfe421e0b519db8dc2f987c5
Dennis Peterson <denispeterson96@gmail.com>
2019-03-29 12:29:56 +0300

    removed a duplicate in urls

--- Base/urls.py
+++ Base/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.urls import include, path
 from Hairways.views import owners, clients, home_views
 
 urlpatterns = [

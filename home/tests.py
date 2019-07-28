12d664a280a1df00cd8286de8c179552161fe431
Dennis Peterson <denispeterson96@gmail.com>
2019-07-29 00:09:35 +0300

    added test

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from home.models import User, Owner, Salons
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    """docstring for UserTestCase"""
+
+    def setUp(self):
+        user = User.objects.create(is_owner=True, nickname="Alexander")
+
+    def test_user_was_created(self):
+        """ Confirms that user was created as expected."""
+        user = User.objects.get(nickname="Alexander")
+        self.assertEqual(str(user.nickname), "Alexander")

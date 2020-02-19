fbbad1a83d732e9094803da9455db901d319dbaf
coderjaymoh <lelerukjaymoh@gmail.com>
2020-02-19 16:50:48 +0300

    Small changes on allowed host

--- base/settings.py
+++ base/settings.py
@@ -20,8 +20,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', "localhost"]
-ALLOWED_HOSTS = '*'
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', "localhost"]
+# ALLOWED_HOSTS = '*'
 
 
 # Application definition

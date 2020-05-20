66d2f181504cc07d553f31a2d54f6f945fb82f88
Dennis Peterson <denispeterson96@gmail.com>
2020-05-20 17:29:32 +0300

    added some few tests for urls

---
+++ home/tests/test_urls.py
@@ -0,0 +1,28 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from home.views import index, salon_details
+from home.clients import ClientSignUpView
+from home.owners import SalonCreateView, OwnerSignUpView
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_home_url_resolves(self):
+        url = reverse('home')
+        self.assertEqual(resolve(url).func, index)
+
+    def test_create_salon_url_resolves(self):
+        url = reverse('new_salon')
+        self.assertEqual(resolve(url).func.view_class, SalonCreateView)
+
+    def test_salon_details_url_resolves(self):
+        url = reverse('salon_details', args=['sample-salon-slug'])
+        self.assertEqual(resolve(url).func, salon_details)
+
+    def test_client_signup_url_resolves(self):
+        url = reverse('client_signup')
+        self.assertEqual(resolve(url).func.view_class, ClientSignUpView)
+
+    def test_owner_signup_url_resolves(self):
+        url = reverse('owner_signup')
+        self.assertEqual(resolve(url).func.view_class, OwnerSignUpView)

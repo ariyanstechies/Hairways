7b156f87b511e0b1d9864b551cd94671461eb389
Dennis Peterson <denispeterson96@gmail.com>
2020-07-24 22:00:17 +0200

    configured s3 for media storage

--- base/settings/prod.py
+++ base/settings/prod.py
@@ -1,5 +1,5 @@
 from base.settings.base import *
-
+import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -14,8 +14,26 @@ SESSION_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = ['.hairwayskenya.com']
 
+INSTALLED_APPS.append(
+    'storages'
+)
+
 # TODO ADD SENTRY
 
+# AWS
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+# Media
+DEFAULT_FILE_STORAGE = 'base.settings.storage_backends.MediaStorage'
 
 # Static
 STATIC_ROOT = os.path.join(BASE_DIR,

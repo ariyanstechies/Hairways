16a8bde15274eedeee94708f35e57feae9d748cd
Dennis Peterson <denispeterson96@gmail.com>
2020-07-25 22:06:06 +0300

    refactoring models

--- base/urls.py
+++ base/urls.py
@@ -1,25 +1,25 @@
 from django.contrib import admin
 from django.urls import include, path
-from home import accounts as account, views
+from home import accounts, views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
 
     # Accounts
-    path('accounts/login/', account.Login.as_view(), name='login'),
-    path('accounts/logout/', account.Logout.as_view(), name='logout'),
-    path('accounts/register/', account.UserSignUpView.as_view(),
+    path('accounts/login/', accounts.Login.as_view(), name='login'),
+    path('accounts/logout/', accounts.Logout.as_view(), name='logout'),
+    path('accounts/register/', accounts.UserSignUpView.as_view(),
          name='register'),
     path('accounts/password_change/',
-         account.PasswordChange.as_view(), name='password_change'),
+         accounts.PasswordChange.as_view(), name='password_change'),
     path('accounts/password_reset/',
-         account.PasswordReset.as_view(), name='password_reset'),
+         accounts.PasswordReset.as_view(), name='password_reset'),
     path('accounts/password_reset/done/',
-         account.PasswordResetDone.as_view(), name='password_reset_done'),
+         accounts.PasswordResetDone.as_view(), name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/',
-         account.PasswordResetConfirm.as_view(), name='password_reset_confirm'),
-    path('accounts/reset/done/', account.PasswordResetComplete.as_view(),
+         accounts.PasswordResetConfirm.as_view(), name='password_reset_confirm'),
+    path('accounts/reset/done/', accounts.PasswordResetComplete.as_view(),
          name='password_reset_complete'),
-    path('accounts/profile/', account.profile_edit, name='profile_edit'),
+    path('accounts/profile/', accounts.profile_edit, name='profile_edit'),
 ]
